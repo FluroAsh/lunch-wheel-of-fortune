306fe3ce114a0a1ae6eb9f53548cbb6418c420e9
Ashley Thompson <workashleythompson@gmail.com>
2025-10-16 23:04:13 +1100

    chore: Copy update

--- src/features/menu/components/map.skeleton.tsx
+++ src/features/menu/components/map.skeleton.tsx
@@ -1,6 +1,6 @@
 export const MapSkeleton = () => (
   <div className="my-2 max-w-[800px] overflow-y-auto rounded-md border border-neutral-300 bg-neutral-800/50 p-2">
-    <p className="font-bold">Places nearby that are currently open</p>
+    <p className="font-bold">Places nearby that are currently OPEN! 🍽️</p>
     {Array.from({ length: 10 }).map((_, index) => (
       <div
         className="my-2 h-4 animate-pulse rounded-md bg-neutral-700/50"
