13b9ae5dd3258e199d4a12ecb4028283d03e922b
Ashley Thompson <workashleythompson@gmail.com>
2025-11-23 21:24:44 +1100

    fix: Adds overflow hidden to wheel component to prevent canvas overflow issues on spin

--- src/features/wheel/components/dynamic-wheel.tsx
+++ src/features/wheel/components/dynamic-wheel.tsx
@@ -6,12 +6,15 @@ import { Wheel } from "react-custom-roulette-r19";
 
 export type WheelProps = ComponentProps<typeof Wheel>;
 
-
 // Lazy over dynamic so we can suspend the component
 const ClientSideSafeWheel = lazy(() =>
   import("react-custom-roulette-r19").then((mod) => ({ default: mod.Wheel })),
 );
 
 export default function DynamicWheel(props: WheelProps) {
-  return <ClientSideSafeWheel {...props} />;
+  return (
+    <div className="overflow-hidden">
+      <ClientSideSafeWheel {...props} />
+    </div>
+  );
 }
