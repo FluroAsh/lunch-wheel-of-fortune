4b9af1d16407bcb93b1aa164b25f954b03ac317c
Ashley Thompson <workashleythompson@gmail.com>
2025-11-30 19:14:32 +1100

    refactor: Small width animation enhancement for autocomplete input

--- src/features/menu/components/address-input/index.tsx
+++ src/features/menu/components/address-input/index.tsx
@@ -9,16 +9,17 @@ export const AutocompleteAddressInput = ({
 }) => {
   return (
     <div className="absolute top-0 right-0 left-0 z-10 w-fit max-w-full p-2 lg:p-4">
-      <div id="address-input-wrapper" className="relative max-w-full">
+      <div id="address-input-wrapper" className="relative">
         <SearchIcon className="absolute top-1/2 left-3 z-10 size-4 -translate-y-1/2 stroke-neutral-400 text-neutral-100" />
         <input
+          id="search-address-input"
           className={cn(
-            "max-w-full rounded-md bg-neutral-900 p-1.5 pl-8.5 text-neutral-100 opacity-80 backdrop-blur-lg",
-            "truncate transition-opacity duration-200",
-            "focus:opacity-100 focus:ring-2 focus:ring-sky-500 focus:outline-none disabled:cursor-not-allowed disabled:opacity-60",
+            "w-[150px] max-w-full rounded-md bg-neutral-900 p-1.5 pl-8.5 text-neutral-100 opacity-80 backdrop-blur-lg",
+            "truncate transition-[opacity,width]",
+            "focus:w-[220px] focus:opacity-100 focus:ring-2 focus:ring-sky-500 focus:outline-none disabled:cursor-not-allowed disabled:opacity-60",
           )}
           type="text"
-          placeholder="Where's the food?"
+          placeholder="Search by address..."
           disabled={isLoading}
         />
 
